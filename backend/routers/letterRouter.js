224321684358ed706abd7adbafada9d47076cd74
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2021-05-21 17:10:41 +0200

    Fréquence de proposition des lettres en fonction de la réussite de chacune

--- backend/routers/letterRouter.js
+++ backend/routers/letterRouter.js
@@ -2,7 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import letters from '../datas/letters.js';
 import Letter from '../models/letterModel.js';
-import { getOneRandom, getRandom } from '../utils/lettersSelection.js';
+import { getOneRandom, getRandom, shuffle } from '../utils/lettersSelection.js';
 
 const letterRouter = express.Router();
 
@@ -16,14 +16,41 @@ letterRouter.get(
 
 letterRouter.get('/:game&:position', expressAsyncHandler(async (req, res) => {
 
-    const letters = await Letter.find({game: req.params.game, position: req.params.position});
+    const letters = await Letter.find({game: req.params.game, position: req.params.position}).lean();
     const data = [];
+    const weightedLetters = [];
+
+    letters.map((letter) => {
+        if(letter.nbTot !== 0){
+            letter.weight = Math.round(((1 - letter.nbSuccess / letter.nbTot) + 1) * 100);
+        } else {
+            letter.weight = 200;
+        }
+        return letter;
+    });
+
+    letters.forEach((letter) => {
+        for(let i = 0; i <= letter.weight; i++){
+            //delete letter.weight;
+            weightedLetters.push(letter);
+        }
+    });
+    
+    weightedLetters.map(letter => {
+        delete letter.weight;
+        return letter;
+    });
     
     for(let i=0; i<20; i++){
         let round = {};
-        const answers = getRandom(letters, 4);
-        const question = getOneRandom(answers);
-        round.answers = answers;
+        const question = getOneRandom(weightedLetters);
+        console.log('question', question);
+        const possibleAnswers = weightedLetters.filter(letter => {
+            return letter._id !== question._id;
+        });
+        const answers = [question, ...getRandom(possibleAnswers, 3)];
+        console.log('answers', answers); // answers peut contenir plusieurs fois la même lettre :(
+        round.answers = shuffle(answers);
         round.question = question;
         data.push(round);
     }
@@ -37,7 +64,7 @@ letterRouter.post('/', expressAsyncHandler(async (req, res) => {
     letters.forEach(async ({ id, score }) => {
         await Letter.findByIdAndUpdate(id, { $inc: { nbSuccess: score, nbTot: 1 } });
     });
-    
+
     res.end();
 }));
 
--- backend/utils/lettersSelection.js
+++ backend/utils/lettersSelection.js
@@ -6,23 +6,6 @@ export const getId = (datas) => {
     return tabIdLetters
 }
 
-  
-export const getRandom = (arr, n) => {
-    var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
-    if (n > len)
-        throw new RangeError("getRandom: more elements taken than available");
-
-    while (n--) {
-        var x = Math.floor(Math.random() * len);
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len in taken ? taken[len] : len;
-    }
-
-    return result;
-}
-
 export const getOneRandom = (arr) => {
     var result,
         len = arr.length;
@@ -30,3 +13,36 @@ export const getOneRandom = (arr) => {
         result = arr[x]
     return result;
 }
+
+export const getRandom = (arr, count) => {
+    let answer = [], counter = 0;
+   
+    while(counter < count){
+      let rand = arr[Math.floor(Math.random() * arr.length)];
+      if(!answer.some(an => an === rand)){
+        answer.push(rand);
+        counter++;
+      }
+    }
+    
+    return answer;
+}
+
+export const shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
